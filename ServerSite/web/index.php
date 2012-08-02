8a26c63a82ee84ee3b5c736608b9a61440e94f82
Vilson D <vilsduka@gmail.com>
2012-08-02 15:56:56 +0200

    Created DatabaseController class

---
+++ ServerSite/web/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Image Hub</title>
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <div id="login">
+            <form action="login.php" method="post" >
+                <label>Username</label>
+                <br/>
+                <input type="text" name="username" />
+                <br/>
+                <label>Password</label>
+                <br/>
+                <input type="password" name="pass" />
+                <br/>
+                <input type="submit" />
+            </form>
+        </div>
+    </body>
+</html>
